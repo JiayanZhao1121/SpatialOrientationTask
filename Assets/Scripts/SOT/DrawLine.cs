24278e019b64440e7f40bd2c693be99bbc1969df
Jiayan Zhao <JIAYANZHAO@outlook.com>
2019-10-09 11:53:24 -0400

    Spatial Orientation Task developed by Unity

---
+++ Assets/Scripts/SOT/DrawLine.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+    private LineRenderer lineRender;
+    private float counter;
+    private float dist;
+    public Color c1 = Color.black;
+    public Color c2 = Color.black;
+    public Transform origin;
+    public Transform circlePivot;
+    private float a;
+    private float b;
+    private float c;
+    private float d;
+    private float e;
+    private float f;
+    private float deltaX;
+    private float deltaY;
+    private float adjustedEndX;
+    private float adjustedEndY;
+    private float adjustedDist;
+    public float Angle;
+    private Vector3 fixedEndPosition;
+    public bool isFixed;
+    
+
+    public float lineDrawSpeed;
+    public Transform ButtonTopLeftPivot;
+    public Transform ButtonBottomRightPivot;
+
+    // Use this for initialization
+    void Start()
+    {
+        lineRender = GetComponent<LineRenderer>();
+        lineRender.startColor = Color.black;
+        lineRender.endColor = Color.black;
+        Angle = 0f;
+        fixedEndPosition = new Vector3();
+        
+        isFixed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((!(Camera.main.ScreenToWorldPoint(Input.mousePosition).x > ButtonTopLeftPivot.position.x &&
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).x < ButtonBottomRightPivot.position.x &&
+             Camera.main.ScreenToWorldPoint(Input.mousePosition).y < ButtonTopLeftPivot.position.y &&
+              Camera.main.ScreenToWorldPoint(Input.mousePosition).y > ButtonBottomRightPivot.position.y)) && isFixed == false)
+        {
+
+
+            Vector3 screenPoint = Input.mousePosition;
+            screenPoint.z = 0f;
+            Vector3 endPosition = new Vector3(Camera.main.ScreenToWorldPoint(screenPoint).x, Camera.main.ScreenToWorldPoint(screenPoint).y,
+                0f);
+            Vector3 startpoint = new Vector3(origin.position.x, origin.position.y, 0f);
+            dist = Vector3.Distance(startpoint, endPosition);
+            Vector3 circlePivotAdjustedPosition = new Vector3(circlePivot.position.x, circlePivot.position.y,
+                0f);
+
+            //
+            a = Vector3.Distance(startpoint, circlePivotAdjustedPosition);
+            //Debug.Log("a=" + a);
+            b = dist;
+            // Debug.Log("dist=" + b);
+            c = startpoint.x;
+            //Debug.Log("c=" + c);
+            d = startpoint.y;
+            //Debug.Log("d=" + d);
+            e = endPosition.x;
+            //Debug.Log("e=" + e);
+            f = endPosition.y;
+            //Debug.Log("f=" + f);
+            deltaX = Mathf.Abs(((e - c) * a) / b);
+            deltaY = Mathf.Abs(((f - d) * a) / b);
+            // 1st quad
+            if (e >= c && f >= d)
+            {
+                adjustedEndX = c + deltaX;
+                adjustedEndY = d + deltaY;
+                float cosValue = deltaX / a;
+                float TAngle = (Mathf.Acos(cosValue) * 180f) / Mathf.PI;
+                Angle = 90f - TAngle;
+                Debug.Log("Angle = " + Angle);
+            }
+            // 4th quad
+            else if (e > c && f < d)
+            {
+                adjustedEndX = c + deltaX;
+                adjustedEndY = d - deltaY;
+                float cosValue = deltaX / a;
+                float TAngle = (Mathf.Acos(cosValue) * 180f) / Mathf.PI;
+                Angle = 90f + TAngle;              
+            }
+            // 2nd quad
+            else if (e < c && f > d)
+            {
+                adjustedEndX = c - deltaX;
+                adjustedEndY = d + deltaY;
+                float cosValue = deltaX / a;
+                float TAngle = (Mathf.Acos(cosValue) * 180f) / Mathf.PI;
+                Angle = 270f + TAngle;
+            }
+            // 3rd quad
+            else if (e < c && f < d)
+            {
+                adjustedEndX = c - deltaX;
+                adjustedEndY = d - deltaY;
+                float cosValue = deltaX / a;
+                float TAngle = (Mathf.Acos(cosValue) * 180f) / Mathf.PI;
+                Angle = 270f - TAngle;
+            }
+
+            //
+
+            lineRender.startColor = c1;
+            lineRender.endColor = c2;
+
+            if (isFixed == false)
+            {
+                lineRender.SetPosition(0, startpoint);
+                counter += 0.1f / lineDrawSpeed;
+                adjustedDist = a;
+                float x = Mathf.Lerp(0, adjustedDist, counter);
+                Vector3 pointA = startpoint;
+                Vector3 adjustedEndPosition = new Vector3(adjustedEndX, adjustedEndY, 0f);
+                Vector3 pointB = adjustedEndPosition;
+
+                Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+                lineRender.SetPosition(1, pointAlongLine);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Angle = " + Angle);
+                isFixed = true;
+                float x = Mathf.Lerp(0, adjustedDist, counter);
+                Vector3 pointA = startpoint;
+                fixedEndPosition = new Vector3(adjustedEndX, adjustedEndY, 0f);
+                Vector3 pointB = fixedEndPosition;
+
+                Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+                lineRender.SetPosition(1, pointAlongLine);
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SOT/SwitchSOTQuestions.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchSOTQuestions : MonoBehaviour {
+    public GameObject SP;
+    public GameObject EQ;
+    public GameObject Q1;
+    public GameObject Q2;
+    public GameObject Q3;
+    public GameObject Q4;
+    public GameObject Q5;
+    public GameObject Q6;
+    public GameObject Q7;
+    public GameObject Q8;
+    public GameObject Q9;
+    public GameObject Q10;
+    public GameObject Q11;
+    public GameObject Q12;
+
+    public GameObject LineDraw;
+    public float timeLimit = 420f;
+    // public GameObject enterParticipantNameGameobject;
+    public Text ParticipantNameText;
+    private int pointingAngle;
+
+    public Text TimerText;
+    private float startTime;
+    private int countForTimer;
+    private void Start()
+    {
+        startTime = 0f;
+        countForTimer = 0;
+    }
+    private void Update()
+    {
+        
+        if (SP.activeSelf || EQ.activeSelf)
+        {
+            LineDraw.SetActive(false);
+        }
+        else
+        {
+            LineDraw.SetActive(true);
+        }
+
+        if (Q1.activeSelf && countForTimer == 0)
+        {
+            startTime = Time.time;
+            countForTimer++;
+        }
+        if (countForTimer != 0)
+        {
+            float remainTime = timeLimit + 0.99f - Time.time + startTime;
+            int min = (int)(remainTime / 60f);
+            int seconds = (int)remainTime - (min * 60);
+            if (seconds < 10)
+            {
+            TimerText.text = min.ToString() + " : " + "0" + seconds.ToString();
+            }
+            else
+            {
+                TimerText.text = min.ToString() + " : " + seconds.ToString();
+            }
+            if (min == 0 && seconds < 1)
+            {
+               // UnityEditor.EditorApplication.isPlaying = false;
+                Application.Quit();
+            }
+
+        }
+    }
+    public void SPToEQ()
+    {
+        
+        if (ParticipantNameText.text != "")
+        {
+ SP.SetActive(false);
+        EQ.SetActive(true);
+        LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+       
+        
+        
+    }
+    public void EQToQ1()
+    {
+        EQ.SetActive(false);
+        Q1.SetActive(true);
+        LineDraw.GetComponent<DrawLine>().isFixed = false;
+    }
+    public void Q1ToQ2()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Debug.Log("pointing angle = " + pointingAngle);
+            Q1.SetActive(false);
+            Q2.SetActive(true);
+            getTheData(123, 1);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+        
+    }
+    public void Q2ToQ3()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Debug.Log("pointing angle = " + pointingAngle);
+            Q2.SetActive(false);
+            Q3.SetActive(true);
+            getTheData(237, 2);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q3ToQ4()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q3.SetActive(false);
+            Q4.SetActive(true);
+            getTheData(83, 3);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q4ToQ5()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q4.SetActive(false);
+            Q5.SetActive(true);
+            getTheData(156, 4);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q5ToQ6()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q5.SetActive(false);
+            Q6.SetActive(true);
+            getTheData(319, 5);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q6ToQ7()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q6.SetActive(false);
+            Q7.SetActive(true);
+            getTheData(235, 6);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q7ToQ8()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q7.SetActive(false);
+            Q8.SetActive(true);
+            getTheData(333, 7);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q8ToQ9()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q8.SetActive(false);
+            Q9.SetActive(true);
+            getTheData(260, 8);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q9ToQ10()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q9.SetActive(false);
+            Q10.SetActive(true);
+            getTheData(280, 9);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q10ToQ11()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q10.SetActive(false);
+            Q11.SetActive(true);
+            getTheData(48, 10);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q11ToQ12()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            Q11.SetActive(false);
+            Q12.SetActive(true);
+            getTheData(26, 11);
+            LineDraw.GetComponent<DrawLine>().isFixed = false;
+        }
+    }
+    public void Q12ToEnd()
+    {
+        if (LineDraw.GetComponent<DrawLine>().isFixed == true)
+        {
+            pointingAngle = (int)LineDraw.GetComponent<DrawLine>().Angle; // valid input
+            getTheData(150, 12);
+           UnityEditor.EditorApplication.isPlaying = false;
+           // Application.Quit();
+        }
+    }
+
+    public void Reset()
+    {
+        LineDraw.GetComponent<DrawLine>().isFixed = false;
+    }
+    void getTheData(float correctAngle, int questionNumber)
+    {
+        string filePath = @"Assets/Resource/SOT_saved_data.csv";
+        //string filePath =  Application.dataPath + "/ExperimentData/SOT_saved_data.csv"; 
+        File.AppendAllText(filePath, ParticipantNameText.text
+            + "," + questionNumber.ToString() + "," +
+            pointingAngle.ToString() + "," + ((int)correctAngle).ToString() + ","
+          + (Mathf.Abs((int)LineDraw.GetComponent<DrawLine>().Angle - (int)correctAngle)).ToString() + "\n");
+    }
+}
